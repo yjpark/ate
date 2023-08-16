7570e0f994bbc44e8fe3f5befbdbf0d52117fd03
YJ Park <yjpark@gmail.com>
2023-08-16 14:35:38 +0800

    🚧 [WIP] - 2023-08-16 14:35:38 -

---
+++ app/src/main.rs
@@ -0,0 +1,73 @@
+#![allow(non_snake_case)]
+
+
+use dioxus_router::prelude::*;
+
+use dioxus::prelude::*;
+use log::LevelFilter;
+
+
+
+
+fn main() {
+    // Init debug
+    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+    
+    dioxus_desktop::launch(app);
+    
+}
+
+
+
+
+
+
+fn app(cx: Scope) -> Element {
+    render!{
+        Router::<Route> {}
+    }
+}
+
+
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+
+
+#[inline_props]
+fn Blog(cx: Scope, id: i32) -> Element {
+    render! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[inline_props]
+fn Home(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+    
+
+    cx.render(rsx! {
+        Link {
+            to: Route::Blog {
+                id: *count.get()
+            },
+            "Go to blog"
+        }
+        div {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
+            
+        }
+    })
+}
+
+
+
