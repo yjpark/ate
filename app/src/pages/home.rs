4654122b99177a575b45dbbe2d4af96e4b429bb2
YJ Park <yjpark@gmail.com>
2023-08-17 10:51:46 +0800

    🚧 [WIP] - 2023-08-17 10:51:46 -

--- app/src/pages/home.rs
+++ app/src/pages/home.rs
@@ -1,8 +1,13 @@
 use dioxus::prelude::*;
+use dioxus_router::prelude::*;
+
+use crate::app::Route;
+use ate_model::prelude::*;
 
 #[inline_props]
 pub fn view(cx: Scope) -> Element {
     render!{
         div { "TODO 4" }
+        Link { to: Route::Entry { id: Uuid::new_v4() }, "Go to entry" }
     }
 }
